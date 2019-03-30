c31a9d7ab06a9f0f3d127173df9be0f2ca693994
Kyler <kyler.figgins@gmail.com>
2019-03-29 22:30:19 -0600

    Added small tweaks

--- myportfolio/src/App.js
+++ myportfolio/src/App.js
@@ -1,23 +1,37 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    direction: "reverse"
+  };
+
+  toggleDirection = () => {
+    this.setState({
+      direction: this.state.direction === "reverse" ? "" : "reverse"
+    });
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+          <p>Hi, My name is Kyler Figgins</p>
+          <img
+            src={logo}
+            className="App-logo"
+            alt="logo"
+            style={{ animationDirection: this.state.direction }}
+          />
+          <p>And I'm a Front-End Developer</p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            onClick={this.toggleDirection}
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Boom!
           </a>
         </header>
       </div>
