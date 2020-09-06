443e990cfd6383160af02a54a6f689cee7318c6a
kfiggins1 <kfiggins@spotinc.com>
2020-09-05 22:16:26 -0400

    added google anayltics to blog

--- blog/fig-blog/gatsby-config.js
+++ blog/fig-blog/gatsby-config.js
@@ -76,6 +76,12 @@ module.exports = {
         pathToConfigModule: `src/utils/typography`,
       },
     },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-177341049-1",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
