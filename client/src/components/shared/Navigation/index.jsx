e6075d159a5167185db4c0f34687a83d22f986ce
kfiggins1 <kfiggins@spotinc.com>
2020-09-07 23:36:22 -0400

    fix menu. links were still working on the left side when menu was closed

--- client/src/components/shared/Navigation/index.jsx
+++ client/src/components/shared/Navigation/index.jsx
@@ -14,7 +14,7 @@ export default function Navagation(props) {
       <label for="nav-toggle" class="navigation__logo">
         <span className="navigation__icon"></span>
       </label>
-      <div className={isMobile ? "navigation__background" :"navigation__background navigation__background__transition"} style={{ backgroundImage: `radial-gradient(${darkColor}, ${lightColor})` }}>&nbsp;</div>
+      <div className="navigation__background" style={{ backgroundImage: `radial-gradient(${darkColor}, ${lightColor})` }}>&nbsp;</div>
       <nav className={isMobile ? "navigation__nav" :"navigation__nav navigation__nav__transition"} >
         <ul className={isMobile ? "navigation__list navigation__list__mobile" : "navigation__list"}>
           {menuItems.map((item, index) =>{
