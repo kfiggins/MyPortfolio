ed84ec61d78484a27f217ed694f850a9081b6798
kfiggins1 <kfiggins@spotinc.com>
2021-01-20 22:18:13 -0500

    finished create book section

--- client/src/components/FavoriteBooks/CreateBook/index.jsx
+++ client/src/components/FavoriteBooks/CreateBook/index.jsx
@@ -1,11 +1,42 @@
 import React, { useState, useEffect, useReducer } from "react";
 
+const createBookFields = [
+  "title",
+  "amazonImageLink",
+  "amazonNestedImageSrc",
+  "amazonBottomImageSrc",
+  "categories",
+  "description",
+  "rank",
+];
+
 export default function CreateBook() {
   const [showCreateForm, setShowCreateForm] = useState(false);
 
   const handleSecretKeyPress = (event) => {
     if (event.ctrlKey && event.altKey && event.key === "c")
       setShowCreateForm(true);
+  };
+
+  const handleSaveBook = async (state) => {
+    let hasValidationError = false;
+    createBookFields.forEach((field) => {
+      if (!state[field].length > 0) hasValidationError = true;
+    });
+    if (hasValidationError) {
+      console.log("Must fill out all of the fields");
+    }
+    await fetch(
+      "https://nsu1exvweg.execute-api.us-east-2.amazonaws.com/default/favoriteBookList",
+      {
+        method: "POST",
+        body: JSON.stringify({
+          ...state,
+          categories: state.categories.split(","),
+        }),
+      }
+    );
+    setShowCreateForm(false);
   };
 
   useEffect(() => {
@@ -17,13 +48,21 @@ export default function CreateBook() {
 
   const [state, dispatch] = useReducer(
     (state, action) => ({ ...state, [action.key]: action.value }),
-    { title: "" }
+    {
+      title: "",
+      amazonImageLink: "",
+      amazonNestedImageSrc: "",
+      amazonBottomImageSrc: "",
+      categories: "",
+      description: "",
+      rank: "",
+    }
   );
 
   return showCreateForm ? (
     <>
       <div>You've unlocked the secret book adding section</div>
-      {["title", "amazonId"].map((item) => (
+      {createBookFields.map((item) => (
         <>
           <label htmlFor={item}>{item}</label>
           <input
@@ -33,6 +72,7 @@ export default function CreateBook() {
           />
         </>
       ))}
+      <button onClick={() => handleSaveBook(state)}>Save</button>
     </>
   ) : null;
 }
